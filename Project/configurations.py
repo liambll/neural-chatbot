bf9b88730894412ad0766c4b6ec210a96aacf7c7
Liam Bui Le Linh <Liam Bui Le Linh>
2017-03-24 22:30:37 -0700

    update

--- Project/configurations.py
+++ Project/configurations.py
@@ -3,17 +3,17 @@
 data_path = 'datasets/twitter/'
 model_path = 'models/twitter/'
 #data_path = 'datasets/cornell-movie/'
-#model_path = 'ckpt/cornell-movie/'
+#model_path = 'models/cornell-movie/'
 vocab_size = 8000
 source_len = 20
 target_len = 20
-layer_size = 512
-num_layers = 3
-model_type = 1 #1 - Seq2Seq; 2 - Seq2Seq with attention
+layer_size = 512 #512
+num_layers = 1  # 3
+model_type = 1 #1 - Seq2Seq; 2 - Seq2Seq with attention # 3 - tied Seq2Seq
 attention_heads = 1
 reverseInput = False
-batch_size = 32
-learning_rate=0.0001
-epochs=100000
+batch_size = 64
+learning_rate=0.001
+epochs=30
 
 
--- Project/run.py
+++ Project/run.py
@@ -5,6 +5,7 @@ Created on Mon Feb 13 19:19:28 2017
 @author: linhb
 """
 
+import six
 import configurations
 import data
 import model_utils
@@ -57,14 +58,11 @@ def main(argv=None):
     sess = model.load_model()
                                
     if (mode.lower() == "train"):
-        val_batch_gen = model_utils.batch_gen(validX, validY, batch_size)
-        train_batch_gen = model_utils.batch_gen(trainX, trainY, batch_size)
-
-        sess = model.train(train_batch_gen, val_batch_gen)
+        sess = model.train(trainX, trainY, validX, validY, batch_size)
     
     elif (mode.lower() == "test"):
-        test_batch_gen = model_utils.batch_gen(testX, testY, 256)
-        input_ = test_batch_gen.__next__()[0]
+        test_batch_gen = model_utils.batch_gen(testX, testY, batch_size)
+        input_ = six.next(test_batch_gen)[0]
         
         output = model.predict(sess, input_)
         
